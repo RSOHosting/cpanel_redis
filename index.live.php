5ba2a510afc03baf23f37bfc35ee2728d4ec2775
Georgi Georgiev <g.georgiev@rso.bg>
2018-11-01 11:05:50 +0200

    Updated page

--- index.live.php
+++ index.live.php
@@ -1,16 +1,25 @@
 <?php
 require_once("/usr/local/cpanel/php/cpanel.php");
-// Create new cpanel object to integrate.
+
+//Create new cpanel object to integrate.
 $cpanel = &new CPANEL();
 
-$res = $cpanel->api1('Branding', 'include', array('stdheader.html') );
-print $res['cpanelresult']['data']['result'];
-?>
-<div class="body-content">
-Hello World!
-</div>
-<?php
-$res = $cpanel->api1('Branding', 'include', array('stdfooter.html') );
-print $res['cpanelresult']['data']['result'];
-$cpanel->end();
+print $cpanel->header( "Redis Manager by RSO Hosting" );      // Add the header.
 
+?>
+
+<div class="body-content">
+<p class="description">
+Redis is an open source (BSD licensed), in-memory data structure store, used as a database, cache and message broker. It supports data structures such as strings, hashes, lists, sets, sorted sets with range queries, bitmaps, hyperloglogs, geospatial indexes with radius queries and streams. 
+</p>
+<div class="section">
+<h2>Redis Status</h2>
+</div>
+</div>
+
+<?php
+
+print $cpanel->footer();
+
+$cpanel->end();
+?> 
