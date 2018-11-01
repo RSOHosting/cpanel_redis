f89f9e29111835aadf1b29ce7d5f16d8a07cb437
Georgi Georgiev <g.georgiev@rso.bg>
2018-11-01 12:48:57 +0200

    Added ssh

--- index.live.php
+++ index.live.php
@@ -18,13 +18,21 @@ echo $cpanelHeader;
 Redis is an open source (BSD licensed), in-memory data structure store, used as a database, cache and message broker. It supports data structures such as strings, hashes, lists, sets, sorted sets with range queries, bitmaps, hyperloglogs, geospatial indexes with radius queries and streams. 
 </p>
 <div class="section">
-<h2>Redis Status</h2>
+<h2>Redis Status</h2> <div class="circle red">Red</div>
   <input  type="checkbox" 
           class="check check1" id="checkbox">
 </div>
 </div>
 
 <?php
+$connection = ssh2_connect('earth.rso.bg', 22, array('hostkey'=>'ssh-rsa'));
+if (ssh2_auth_pubkey_file($connection, 'root',
+                          'id_rsa.pub',
+                          'id_rsa')) {
+  echo "Public Key Authentication Successful\n";
+} else {
+  die('Public Key Authentication Failed');
+}
 
 print $cpanel->footer();
 
