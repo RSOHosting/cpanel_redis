8a45626d8e298e7b845e5d95f03a4848bd724809
KuttySoft MY <admin@kuttysoft.com>
2024-02-23 22:32:47 +0800

    Update install.sh
    Update to new Cpanel Version & Jupiter Theme

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
-rm -fR /usr/local/cpanel/base/frontend/paper_lantern/rso_redis
-mkdir /usr/local/cpanel/base/frontend/paper_lantern/rso_redis
-cd /usr/local/cpanel/base/frontend/paper_lantern/rso_redis
+# Remove existing directory if it exists
+rm -fR /usr/local/cpanel/base/frontend/jupiter/rso_redis
 
+# Create new directory
+mkdir /usr/local/cpanel/base/frontend/jupiter/rso_redis
+cd /usr/local/cpanel/base/frontend/jupiter/rso_redis
+
+# Download plugin
 echo "Downloading plugin..."
 wget -q https://github.com/RSOHosting/cpanel_redis/archive/master.zip -O rso_redis.zip
 
@@ -14,12 +18,12 @@ mv cpanel_redis-master/* ./
 mv cpanel_redis-master/.* ./
 
 # Register plugin with cPanel
-/usr/local/cpanel/scripts/install_plugin /usr/local/cpanel/base/frontend/paper_lantern/rso_redis --theme paper_lantern
+/usr/local/cpanel/scripts/install_plugin /usr/local/cpanel/base/frontend/jupiter/rso_redis --theme jupiter
 
-#Cleanup by removing release
+# Cleanup by removing release
 echo "Cleaning Up..."
 rm -f rso_redis.zip
 
 # Fix permissions
 echo "Finalizing permissions..."
-chmod -R 755 /usr/local/cpanel/base/frontend/paper_lantern/rso_redis
+chmod -R 755 /usr/local/cpanel/base/frontend/jupiter/rso_redis
